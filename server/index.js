b461f3460e1c501a771f110f0653b82ba9ccd468
Genne Liu <genneliu@gmail.com>
2021-09-13 13:12:26 -0700

    backend server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.json()) 
+
+
+app.get("/api/users", (req, res) => {
+    let friends = ['Nitin', 'Eric', 'Jeddy', 'Cameron', 'Riley']; 
+    res.status(200).send(friends);
+})
+
+app.get("/weather/:temperature", (req, res) => {
+    const { temperature } = req.params;
+    const phrase = `<h3> It was ${temperature} today</h3>`;
+    res.status(200).send(phrase);
+})
+
+const SERVER_PORT = 4200;
+app.listen(4200, () => console.log("Server running on port 4200"));
+
